28cdd21c5d88e35ea52f3f58b58b45a1b8c04910
SemperFenix <dureivan@gmail.com>
2023-02-22 23:23:35 +0100

    Remove unnecessary code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,23 +1,11 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { phoneReducer } from "../reducer/phone.reducer";
 
 export const store = configureStore({
   reducer: {
     phone: phoneReducer,
   },
-  preloadedState: {
-    phone: {
-      number: "1234",
-      isCalling: false,
-    },
-  },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
