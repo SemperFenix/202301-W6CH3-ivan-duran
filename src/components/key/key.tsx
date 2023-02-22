368e4c42c723829268ec4ab0b6ef28c44bbabeac
SemperFenix <dureivan@gmail.com>
2023-02-22 23:33:16 +0100

    Add key test

--- src/components/key/key.tsx
+++ src/components/key/key.tsx
@@ -9,7 +9,7 @@ export function Key() {
     inputNumber(element.value);
   };
 
-  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
 
   return (
     <ol className="keyboard">
